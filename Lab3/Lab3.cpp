d60d460b1566ec296ab4689e074a402e96e7eea8
Kate1928 <malininaekaterina8005@gmail.com>
2023-11-10 12:09:37 +0300

    3

--- Lab3/Lab3.cpp
+++ Lab3/Lab3.cpp
@@ -8,7 +8,8 @@
 #include <ctime>
 #include <fstream>
 #include <cmath>
-#include <set>
+#include <map>
+#include <algorithm>
 
 using namespace std;
 
@@ -20,25 +21,14 @@ struct point
     float y;
 };
 
-//struct RGB
-//{
-//    BYTE r;
-//    BYTE g;
-//    BYTE b;
-//};
+//vector <point> stA;
+//int stDif;
+//int stB;
 
 HWND sHwnd;
 HDC hdc;
 int metricsDDA = 0, metricsBrezenham = 0, metricsA = 0, metricsB = 0;
-ofstream file2;
-
-//void coordinateSystem()
-//{
-//    
-//}
-//
-//set <point> stA;
-//set <point> stB;
+//ofstream file2;
 
 //здесь просто все наши координаты, цвета и затравочные точки 
 
@@ -240,56 +230,35 @@ void init(vector<vector<point>>& shapeAll, vector<point>& zPointAll, vector<COLO
 
 //Алгоритм растеризации отрезка «А»: алгоритм ЦДА (DDA) –алгоритм цифрового дифференциального анализатора)
 
-void DDA(vector<point>& shape1, COLORREF color)
+void DDA(int x1, int x2, int y1, int y2, COLORREF color)
 {
     float i, l;
     float x, y, dx, dy;
-    for (int i = 0; i < shape1.size() - 1; i++)
-    {
-        //вычисляем большую из длин l отрезка по оси x и y
-        l = abs(shape1[i + 1].x - shape1[i].x) >= abs(shape1[i + 1].y - shape1[i].y) ? abs(shape1[i + 1].x - shape1[i].x) : abs(shape1[i + 1].y - shape1[i].y);
-        //определяем приращения по оси x и y, полагая большее из них равным единице
-        dx = (shape1[i + 1].x - shape1[i].x) / l;
-        dy = (shape1[i + 1].y - shape1[i].y) / l;
-        //начинаем с точки (x,y)
-        x = shape1[i].x;
-        y = shape1[i].y;
-
-        //цикл по большей длине отрезка
-        for (int j = 0; j < l; j++)
-        {
-            SetPixel(hdc, round(x), round(y), color);
-            //Sleep(100);
-            metricsDDA++;
-            x += dx;
-            y += dy;
-        }
-        file2 << " кол-во пикселейDDA: " << metricsDDA << " ";
-        metricsDDA = 0;
-    }
-    //тоже-самое для последней стороны многоугольника 
-    int end = shape1.size() - 1;
-    l = abs(shape1[0].x - shape1[end].x) >= abs(shape1[0].y - shape1[end].y) ? abs(shape1[0].x - shape1[end].x) : abs(shape1[0].y - shape1[end].y);
+    //вычисляем большую из длин l отрезка по оси x и y
+    l = abs(x2 - x1) >= abs(y2 - y1) ? abs(x2 - x1) : abs(y2 - y1);
     //определяем приращения по оси x и y, полагая большее из них равным единице
-    dx = (shape1[0].x - shape1[end].x) / l;
-    dy = (shape1[0].y - shape1[end].y) / l;
+    dx = (x2 - x1) / l;
+    dy = (y2 - y1) / l;
     //начинаем с точки (x,y)
-    x = shape1[end].x;
-    y = shape1[end].y;
+    x = x1;
+    y = y1;
     //цикл по большей длине отрезка
     for (int j = 0; j < l; j++)
     {
         SetPixel(hdc, round(x), round(y), color);
+        //Sleep(100);
         metricsDDA++;
-        //SetPixel(hdc, round(x), round(y), RGB(255, 0, 0));
         x += dx;
         y += dy;
     }
-    file2 << " кол-во пикселейDDA: " << metricsDDA << endl;
+    //file2 << " кол-во пикселейDDA: " << metricsDDA << " ";
     metricsDDA = 0;
+    
 }
 
-void DDApoint(vector<point>& shape1, COLORREF color, int choice)
+//Это мой доп (пунктир)
+
+void DDApoint(vector<point>& shape1, COLORREF color)
 {
     float i, l;
     float x, y, dx, dy;
@@ -320,7 +289,7 @@ void DDApoint(vector<point>& shape1, COLORREF color, int choice)
             x += dx;
             y += dy;
         }
-        file2 << " кол-во пикселейDDA: " << metricsDDA << " ";
+        //file2 << " кол-во пикселейDDA: " << metricsDDA << " ";
         metricsDDA = 0;
     }
     //тоже-самое для последней стороны многоугольника 
@@ -335,7 +304,7 @@ void DDApoint(vector<point>& shape1, COLORREF color, int choice)
     //цикл по большей длине отрезка
     for (int j = 0; j < l; j++)
     {
-        if (j % choice == 0)
+        if (j % 2 == 0)
         {
             SetPixel(hdc, round(x), round(y), 0x00FFFFFF);
         }
@@ -348,85 +317,26 @@ void DDApoint(vector<point>& shape1, COLORREF color, int choice)
         x += dx;
         y += dy;
     }
-    file2 << " кол-во пикселейDDA: " << metricsDDA << endl;
+    //file2 << " кол-во пикселейDDA: " << metricsDDA << endl;
     metricsDDA = 0;
-}
-
-void DDAtest(int x1, int x2, int y1, int y2)
-{
-    float i, l;
-    float x, y, dx, dy;
-    l = abs(x2 - x1) >= abs(y2 - y1) ? abs(x2 - x1) : abs(y2 - y1);
-    //определяем приращения по оси x и y, полагая большее из них равным единице
-    dx = (x2 - x1) / l;
-    dy = (y2 - y1) / l;
-    //начинаем с точки (x,y)
-    x = x1;
-    y = y1;
-    //цикл по большей длине отрезка
-    for (int j = 0; j < l; j++)
-    {
-        SetPixel(hdc, round(x), round(y), 0x000000FF);
-        metricsDDA++;
-        x += dx;
-        y += dy;
-    }
 }
 
 //Алгоритм растеризации отрезка «Б»: алгоритм Брезенхема для отрезка;
 
-void brezenham(vector<point>& shape1, COLORREF color)
+void brezenham(int x1, int x2, int y1, int y2, COLORREF color)
 {
     float x, y, dx, dy, signX, signY;
     int error, error2;
-    for (int i = 0; i < shape1.size() - 1; i++)
-    {
-        x = shape1[i].x;
-        y = shape1[i].y;
-        dx = abs(shape1[i + 1].x - shape1[i].x);
-        dy = abs(shape1[i + 1].y - shape1[i].y);
-        signX = shape1[i].x < shape1[i + 1].x ? 1 : -1; //1
-        signY = shape1[i].y < shape1[i + 1].y ? 1 : -1; //1
-        error = dx - dy;
-        //SetPixel(hdc, round(shape1[i + 1].x), round(shape1[i + 1].y), color);
-        //metricsBrezenham++;
-        while (x != shape1[i + 1].x || y != shape1[i + 1].y)
-        {
-            if (GetPixel(hdc, round(x), round(y)) == 0x00CCCCCC)
-            {
-                SetPixel(hdc, round(x), round(y), 0x00FFFFFF);
-            }
-            else
-            {
-                SetPixel(hdc, round(x), round(y), color);
-            }
-            metricsBrezenham++;
-            error2 = error * 2;
-            if (error2 > -dy)
-            {
-                error -= dy;
-                x += signX;
-            }
-            if (error2 < dx)
-            {
-                error += dx;
-                y += signY;
-            }
-        }
-        file2 << " кол-во пикселейBr: " << metricsBrezenham << " ";
-        metricsBrezenham = 0;
-    }
-    int end = shape1.size() - 1;
-    x = shape1[end].x;
-    y = shape1[end].y;
-    dx = abs(shape1[0].x - shape1[end].x);
-    dy = abs(shape1[0].y - shape1[end].y);
-    signX = shape1[end].x < shape1[0].x ? 1 : -1; //1
-    signY = shape1[end].y < shape1[0].y ? 1 : -1; //1
+    x = x1;
+    y = y1;
+    dx = abs(x2 - x1);
+    dy = abs(y2 - y1);
+    signX = x1 < x2 ? 1 : -1; //1
+    signY = y1 < y2 ? 1 : -1; //1
     error = dx - dy;
-    SetPixel(hdc, round(shape1[0].x), round(shape1[0].y), color);
-    metricsBrezenham++;
-    while (x != shape1[0].x || y != shape1[0].y)
+    //SetPixel(hdc, round(shape1[i + 1].x), round(shape1[i + 1].y), color);
+    //metricsBrezenham++;
+    while (x != x2 || y != y2)
     {
         if (GetPixel(hdc, round(x), round(y)) == 0x00CCCCCC)
         {
@@ -449,42 +359,8 @@ void brezenham(vector<point>& shape1, COLORREF color)
             y += signY;
         }
     }
-    file2 << " кол-во пикселейBr: " << metricsBrezenham << endl;
+    //file2 << " кол-во пикселейBr: " << metricsBrezenham << " ";
     metricsBrezenham = 0;
-}
-
-void brezenhamTest(int x1, int x2, int y1, int y2)
-{
-    float x, y, dx, dy, signX, signY;
-    int error, error2;
-    x = x1;
-    y = y1;
-    dx = abs(x2 - x1);
-    dy = abs(y2 - y1);
-    signX = x1 < x2 ? 1 : -1; //1
-    signY = y1 < y2 ? 1 : -1; //1
-    error = dx - dy;
-    //auto color = 0x0000FF00;// | GetPixel(hdc, round(x), round(y));
-    SetPixel(hdc, round(x2), round(y2), 0x0000FF00);
-    metricsBrezenham++;
-    while (x != x2 || y != y2)
-    {
-        /*color = GetPixel(hdc, round(x), round(y));
-        color = color | 0x0000FF00;*/
-        SetPixel(hdc, round(x), round(y), 0x0000FF00);
-        metricsBrezenham++;
-        error2 = error * 2;
-        if (error2 > -dy)
-        {
-            error -= dy;
-            x += signX;
-        }
-        if (error2 < dx)
-        {
-            error += dx;
-            y += signY;
-        }
-    }
 }
 
 //Алгоритм растеризации сплошной области «А»: рекурсивный алгоритм заливки внутренне - определённой 4 - х связной области;
@@ -500,7 +376,13 @@ void areaA(int xz, int yz, COLORREF color)
     {
         p = stack.top();
         stack.pop();
-        SetPixel(hdc, p.x, p.y, color);       
+        SetPixel(hdc, p.x, p.y, color);  
+        //auto it = find_if(stA.begin(), stA.end(), [&](const point& s)->bool { return (s.x == p.x && s.y == p.y); });
+        /*if (auto it = find_if(stA.begin(), stA.end(), [&](const point& s)->bool { return (s.x == p.x && s.y == p.y); }); it != end(stA))
+        {
+
+        }  */  //если такого элемента еще нет, то добавляем его
+        //stA.push_back(p);
         metricsA++;
         int x = p.x;
         int y = p.y;
@@ -599,17 +481,22 @@ int main()
     unsigned int start_time; 
     unsigned int end_time;
     unsigned int search_time;
-    ofstream file;
+    //ofstream file;
     
-    file.open("File1.txt");
-    file2.open("File2.txt");
+    //file.open("File1.txt");
+    //file2.open("File2.txt");
 
     int test = 1;
     if (test == 0)
-    {        
+    {
+        
         for (int i = 0; i < 8; i++)
         {
-            DDA(shapeAll[i], colorAll[i]);
+            for (int j = 0; j < shapeAll[i].size() - 1; j++)
+            {
+                brezenham(shapeAll[i][j].x, shapeAll[i][j + 1].x, shapeAll[i][j].y, shapeAll[i][j + 1].y, colorAll[i]);
+            }
+            brezenham(shapeAll[i][shapeAll[i].size() - 1].x, shapeAll[i][0].x, shapeAll[i][shapeAll[i].size() - 1].y, shapeAll[i][0].y, colorAll[i]);
             areaB(zPointAll[i].x, zPointAll[i].y, colorAll[i]);
         }
     }
@@ -617,54 +504,58 @@ int main()
     {
         for (int i = 0; i < 8; i++)
         {
-            DDA(shapeAll[i], colorAll[i]);
+            for (int j = 0; j < shapeAll[i].size() - 1; j++)
+            {
+                DDA(shapeAll[i][j].x, shapeAll[i][j + 1].x, shapeAll[i][j].y, shapeAll[i][j + 1].y, colorAll[i]);
+            }
+            DDA(shapeAll[i][shapeAll[i].size() - 1].x, shapeAll[i][0].x, shapeAll[i][shapeAll[i].size() - 1].y, shapeAll[i][0].y, colorAll[i]);
             start_time = GetTickCount(); // начальное время
             areaA(zPointAll[i].x, zPointAll[i].y, colorAll[i]);
             end_time = GetTickCount(); // конечное время
             search_time = end_time - start_time; // искомое время
-            file << "искомое времяA:" << search_time << " кол-во пикселейA: " << metricsA << " ";
+            //file << "искомое времяA:" << search_time << " кол-во пикселейA: " << metricsA << " ";
             cout << "A:" << search_time << " ";
             metricsA = 0;
 
-            brezenham(shapeAll[i], 0x00FF0000);
+            for (int j = 0; j < shapeAll[i].size() - 1; j++)
+            {
+                brezenham(shapeAll[i][j].x, shapeAll[i][j + 1].x, shapeAll[i][j].y, shapeAll[i][j + 1].y, 0x00FF0000);
+            }
+            brezenham(shapeAll[i][shapeAll[i].size() - 1].x, shapeAll[i][0].x, shapeAll[i][shapeAll[i].size() - 1].y, shapeAll[i][0].y, 0x00FF0000);
+            //brezenham(shapeAll[i], 0x00FF0000);
             start_time = clock(); // начальное время
             areaB(zPointAll[i].x, zPointAll[i].y, 0x00FF0000);
             end_time = clock(); // конечное время
             search_time = end_time - start_time; // искомое время
-            file << "искомое времяB:" << search_time << " кол-во пикселейB: " << metricsB << " ";
+            //file << "искомое времяB:" << search_time << " кол-во пикселейB: " << metricsB << " ";
             cout << "B:" << search_time << " ";
             //cout << "difference:" << stA.size() - stB.size() << " ";
-            file << endl;
+            //file << endl;
             metricsB = 0;
         }
         cout << endl;
         metricsDDA = 0;
         metricsBrezenham = 0;
-        
 
         //----------------------------------------------------------------------
         
         float x2 = 0;
         float y2 = 0;
-        int a, L = 50, x, y;
+        int a, L = 85, x, y;
         for (int i = 0; i < 10; i++)
         {
             a = i * 10;
             x2 = 100 + cos(a * PI / 180) * L;
             y2 = 200 + sin(a * PI / 180) * L;
-            /*if (i != 0)
-            {
-                y2 = 200 - y2;
-            }*/
-            //line[0] = p;
             start_time = clock(); // начальное время
-            DDAtest(100, round(x2), 200, round(y2));
+            DDA(100, round(x2), 200, round(y2), 0x000000FF);
             end_time = clock(); // конечное время
             search_time = end_time - start_time; // искомое время
             cout << "DDA:" << search_time << " ";
             metricsDDA = 0;
             start_time = clock(); // начальное время
-            brezenhamTest(100, round(x2), 200, round(y2));
+            brezenham(100, round(x2), 200, round(y2), 0x0000FF00);
+            //brezenhamTest(100, round(x2), 200, round(y2));
             end_time = clock(); // конечное время
             search_time = end_time - start_time; // искомое время
             cout << "Brezenham:" << search_time << " ";
@@ -674,12 +565,12 @@ int main()
 
     if (test == 2)
     {
-        int choice;
+        /*int choice;
         cout << "point or dot-dash?: 2/5 ";
-        cin >> choice;
+        cin >> choice;*/
         for (int i = 0; i < 8; i++)
         {
-            DDApoint(shapeAll[i], colorAll[i], choice);
+            DDApoint(shapeAll[i], colorAll[i]);
             areaB(zPointAll[i].x, zPointAll[i].y, colorAll[i]);
         }
     }
